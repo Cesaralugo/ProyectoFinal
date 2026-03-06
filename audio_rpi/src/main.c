1ea7dc5b4163a791b69f4f8f39e1a498b856ff66
Samir <samirbarce@gmail.com>
2026-03-05 21:05:56 -0500

    more changes

--- audio_rpi/src/main.c
+++ audio_rpi/src/main.c
@@ -16,17 +16,21 @@ int main()
 {
     Delay delay;
     Delay_init(&delay, 20.0f, 0.5f, 0.4f);
+
     Overdrive od;
     Overdrive_init(&od, 3.0f, 0.7f, 0.9f);
+
     Wah wah;
     Wah_init(&wah, 2.0f, 3.0f, 0.9f);
+
     Chorus ch;
     Chorus_init(&ch,0.8f, 0.7f, 0.5f);
+
     Flanger flanger;
-    Flanger_init(&flanger, 0.25f, 0.7f, 0.3f, 0.5f); // rate, depth, feedback, mix
+    Flanger_init(&flanger, 0.25f, 0.7f, 0.3f, 0.5f); 
 
     PitchShifter pitch;
-    PitchShifter_init(&pitch, 7.0f, 0.5f); // +7 semitonos, mix 50%
+    PitchShifter_init(&pitch, 7.0f, 0.5f);
 
 
     socket_init();
@@ -34,10 +38,10 @@ int main()
     for (int i = 0; i < SAMPLE_RATE; i++)
     {
         float input = (sinf(2.0f * PI * 440.0f * i / SAMPLE_RATE) > 0) ? 1.0f : -1.0f;  
-        //float od_out = Overdrive_process(&od, input);
-        //float ch_out = Chorus_process(&ch, input);
 
-        float post = Chorus_process(&ch, input);
+        float od_out  = Overdrive_process(&od, input);  
+        float wah_out = Wah_process(&wah, od_out);       
+        float post    = Chorus_process(&ch, wah_out);    
 
         socket_send_two_floats(input, post);
         usleep(1000);
